f23ba1ca9790cf4296f776167f045b93e10b2afd
pete3n <pete3n@protonmail.com>
2024-02-10 19:45:56 -0500

    feat: Angry Oxide version bump to 0.8.5

--- pkgs/AngryOxide/default.nix
+++ pkgs/AngryOxide/default.nix
@@ -11,10 +11,10 @@ rustPlatform.buildRustPackage rec {
     owner = "Ragnt";
     repo = pname;
     rev = "v${version}";
-    sha256 = "";
+    sha256 = "sha256-uVZ/tqN8Fw2P4Ue35jN8ZLodMIth6eZsJbgSsdbyqjE=";
   };
 
-  cargoSha256 = "";
+  cargoSha256 = "sha256-iBlAz9Bd3zD2wlm7oMEfeXxEoaE5ELH824gk0fmXajU=";
 
   meta = with lib; {
     description = "A 802.11 Attack tool built in Rust";
