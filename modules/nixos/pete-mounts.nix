b06ac548ceb6cb81da667499686d40dd8af8013d
pete3n <pete3n@protonmail.com>
2024-03-09 17:40:17 -0500

    feat: Added NFS mount

--- modules/nixos/pete-mounts.nix
+++ modules/nixos/pete-mounts.nix
@@ -1,6 +1,15 @@
 {pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    nfs-utils
+  ];
+
   fileSystems."/data" = {
     device = "/dev/disk/by-uuid/a0f72048-c503-4002-a69e-05cba47bf75b";
     fsType = "ext4";
+  };
+  fileSystems."/mnt/nfs" = {
+    device = "192.168.1.16:/mnt/user/share";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto"];
   };
 }
