387571f8530a18bf39367ce8d23c4b4279b9f85d
pete3n <pete3n@protonmail.com>
2024-03-26 13:03:57 -0400

    chore: moving /data mount to appropriate config

--- modules/nixos/pete-mounts.nix
+++ modules/nixos/pete-mounts.nix
@@ -3,10 +3,6 @@
     nfs-utils
   ];
 
-  fileSystems."/data" = {
-    device = "/dev/disk/by-uuid/a0f72048-c503-4002-a69e-05cba47bf75b";
-    fsType = "ext4";
-  };
   fileSystems."/mnt/nfs" = {
     device = "192.168.1.16:/mnt/user/share";
     fsType = "nfs";
