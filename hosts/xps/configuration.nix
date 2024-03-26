9c67de00e843167547d67fb0b0a87b226647440b
pete3n <pete3n@protonmail.com>
2024-03-26 13:06:22 -0400

    chore: moved from mounts modules

--- hosts/xps/configuration.nix
+++ hosts/xps/configuration.nix
@@ -23,6 +23,11 @@
     loader.efi.canTouchEfiVariables = true;
   };
 
+  fileSystems."/data" = {
+    device = "/dev/disk/by-uuid/a0f72048-c503-4002-a69e-05cba47bf75b";
+    fsType = "ext4";
+  };
+
   nix = {
     settings.experimental-features = ["nix-command" "flakes"];
 
