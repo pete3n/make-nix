fd7b5b3e09f2ec9dafb0b5dfb50c1098e8f47b6b
pete3n <pete3n@protonmail.com>
2024-08-31 18:28:59 -0400

    chore: remove teams-for-linux for EOL Electron

--- modules/home-manager/pete/messengers.nix
+++ modules/home-manager/pete/messengers.nix
@@ -7,6 +7,6 @@
     unstable.element-desktop
     unstable.signal-desktop
     unstable.skypeforlinux
-    unstable.teams-for-linux
+    #unstable.teams-for-linux Electron 29.4.6 EOL marked as insecure
   ]);
 }
