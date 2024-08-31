f43fbf0eafd57f4562487e2e36d331adfb6b2fb7
pete3n <pete3n@protonmail.com>
2024-08-31 18:33:05 -0400

    fix: Pin driver 555.58.02 for Kernel 6.10 compatibility

--- modules/nixos/framework16/spc_framework16_awesome_egpu.nix
+++ modules/nixos/framework16/spc_framework16_awesome_egpu.nix
@@ -9,7 +9,14 @@
     system.nixos.tags = ["AwesomeWM" "Aorus-eGPU" "RTX-3080"];
 
     hardware.nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "555.58.02";
+        sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+        sha256_aarch64 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+        openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        persistencedSha256 = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      };
       modesetting.enable = false;
       powerManagement.enable = false;
       open = true;
