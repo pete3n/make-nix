692064715285f282d511d70350ec29968aec242d
pete3n <pete3n@protonmail.com>
2024-09-02 15:55:33 -0400

    chore: cleanup git config

--- modules/home-manager/pete/pete-config.nix
+++ modules/home-manager/pete/pete-config.nix
@@ -38,12 +38,31 @@
     userEmail = "pete3n@protonmail.com";
     extraConfig = {
       core.editor = "nvim";
-      #commit.gpgsign = true;
-      #gpg.format = "ssh";
-      #gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signer";
-      #user.signingkey = "~/.ssh/pete3n.pub";
+      init = {
+        defaultBranch = "main"; # Github default
+        templateDir = "${config.home.homeDirectory}/.git-templates";
+      };
     };
   };
+
+  home.file.".git-templates/gitlint".text = ''
+    [general]
+    # Ignore rules, reference them by id or name (comma-separated)
+    ignore=title-trailing-punctuation, T3
+
+    # Enable specific community contributed rules
+    contrib=contrib-title-conventional-commits,CC1
+
+    # Set the extra-path where gitlint will search for user defined rules
+    #extra-path=./gitlint_rules/my_rules.py
+
+    ### Configuring rules ###
+    [title-max-length]
+    line-length=80
+
+    [title-min-length]
+    min-length=5
+  '';
 
   programs.bash = {
     initExtra = let
@@ -53,9 +72,15 @@
       bash
       */
       ''
-        if command -v keychain > /dev/null 2>&1; then
-        	eval $(keychain --eval --nogui ${ssh-private-key} --quiet);
-        fi
+              if command -v keychain > /dev/null 2>&1; then
+              	eval $(keychain --eval --nogui ${ssh-private-key} --quiet);
+              fi
+
+              set -o vi
+
+              alias screenshot=grim
+              alias ls=lsd
+        alias lsc='lsd --classic'
       '';
 
     profileExtra =
@@ -63,8 +88,7 @@
       bash
       */
       ''
-            # User aliases
-        alias screenshot=grim
+        export EDITOR=nvim
             # Workaround for xdg.userDirs bug always being set to false
             source "${config.home.homeDirectory}/.config/user-dirs.dirs"
       '';
