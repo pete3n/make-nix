548b7f9dbc92f4b2265527be271a54e2f100812a
pete3n <pete3n@protonmail.com>
2024-09-03 11:50:44 -0400

    feat: Set kernel to 6_6 for LTS
    Signed-off-by: pete3n <pete3n@protonmail.com>

--- hosts/framework16/configuration.nix
+++ hosts/framework16/configuration.nix
@@ -19,7 +19,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_6_10;
+    kernelPackages = pkgs.linuxPackages_6_6;
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     supportedFilesystems = ["ntfs"];
