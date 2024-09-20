518179e92bd5836815d1137a30b13746af640e78
pete3n <pete3n@protonmail.com>
2024-09-20 15:41:33 -0400

    feat: quirks module

--- modules/home-manager/pete/default.nix
+++ modules/home-manager/pete/default.nix
@@ -1,16 +1,18 @@
 {
   alacritty-config = import ./alacritty-config.nix;
   awesome-config = import ./awesome-config.nix;
-  user-config = import ./pete-config.nix;
   crypto = import ./crypto.nix;
   firefox-config = import ./firefox.nix;
+  games = import ./games.nix;
   hyprland-config = import ./hyprland-config.nix;
   media-tools = import ./media-tools.nix;
   messengers = import ./messengers.nix;
   misc-tools = import ./misc-tools.nix;
+  neovim-env = import ./neovim-env.nix;
   office-cloud = import ./office-cloud.nix;
   pen-tools = import ./pen-tools.nix;
-  neovim-env = import ./neovim-env.nix;
   theme-style = import ./theme-style.nix;
   tmux-config = import ./tmux-config.nix;
+  user-config = import ./pete-config.nix;
+  quirks = import ./quirks.nix;
 }
