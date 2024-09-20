0c0361fafa4c1e9cbe45f0d97f500e054dae2208
pete3n <pete3n@protonmail.com>
2024-09-20 15:38:49 -0400

    feat: add usrp-sdr module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -2,7 +2,6 @@
 {
   console = import ./console.nix;
   framework16-modules = import ./framework16;
-  gaming = import ./gaming.nix;
   getac-modules = import ./getac;
   iptables-default = import ./iptables-default-rules.nix;
   nvidia-scripts = import ./nvidia-scripts.nix;
@@ -10,6 +9,7 @@
   pete-printer = import ./pete-printer.nix;
   pete-services = import ./pete-services.nix;
   system-tools = import ./system-tools.nix;
+  usrp-sdr = import ./usrp-sdr.nix;
   X11-tools = import ./X11-tools.nix;
   xps-modules = import ./xps;
   yubi-smartcard = import ./yubikey-sc.nix;
