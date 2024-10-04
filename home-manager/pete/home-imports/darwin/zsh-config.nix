2c37100d0f491e54cf19b65aa124562f6907ccee
pete3n <pete3n@protonmail.com>
2024-10-04 12:32:47 -0400

    fix: insecure path warning feat: ohmyzsh plugins chore: alias cleanup

--- home-manager/pete/home-imports/darwin/zsh-config.nix
+++ home-manager/pete/home-imports/darwin/zsh-config.nix
@@ -1,5 +1,4 @@
-{ ... }:
-{
+{pkgs, ...}: {
   programs.bash.enable = false;
   programs.zsh = {
     enable = true;
@@ -7,24 +6,38 @@
     autosuggestion.enable = false;
     syntaxHighlighting.enable = true;
     defaultKeymap = "viins";
+    shellAliases = {
+      lsc = "lsd --classic"; # For annoying colors on SMB/NFS mounts
+      wl-copy = "pbcopy"; # I use Wayland too much to remember the pb clip cmds
+      wl-paste = "pbpaste";
+      cd = "z";
+    };
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "colored-man-pages"
+      ];
+      theme = "robbyrussell";
+    };
+    initExtraFirst =
+      #bash
+      ''
+        # Ignore unsafe directory warnings from Darwin
+        ZSH_DISABLE_COMPFIX="true"
+      '';
     profileExtra =
       # bash
       ''
-        export EDITOR=nvim
-
-        # Show fastfetch at login
+        # Show fastfetch at login but not for every new TMUX pain/window
         if [ -z "$FASTFETCH_EXECUTED" ] && [ -z "$TMUX" ]; then
         	export FASTFETCH_EXECUTED=1
-        	command -v fastfetch &> /dev/null && fastfetch
+        	command -v ${pkgs.fastfetch}/bin/fastfetch &> /dev/null &&
+        	${pkgs.fastfetch}/bin/fastfetch
         fi
-      '';
-    initExtra =
-      # bash
-      ''
-        alias ls=lsd
-        alias lsc='lsd --classic'
-        alias wl-copy='pbcopy'
-        alias wl-paste='pbpaste'
       '';
   };
 }
