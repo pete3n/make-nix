efb25015c6254a154f29550ddb2d844f770fad15
pete3n <pete3n@protonmail.com>
2024-10-05 20:18:50 -0400

    fix: stateVersion vi-mode

--- home-manager/pete/darwin-home.nix
+++ home-manager/pete/darwin-home.nix
@@ -78,7 +78,7 @@
     zathura = {
       enable = true;
     };
-		# Zoxide better cd replacement with memory
+    # Zoxide better cd replacement with memory
     zoxide = {
       enable = true;
       enableZshIntegration = true;
@@ -94,6 +94,8 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
     username = "pete";
     homeDirectory = "/Users/pete";
 
@@ -103,16 +105,5 @@
         local.yubioath-darwin
         python312Packages.base58
       ]);
-    };
-
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "24.05";
   };
 }
--- home-manager/pete/linux-home.nix
+++ home-manager/pete/linux-home.nix
@@ -36,6 +36,8 @@
   fonts.fontconfig.enable = true;
 
   home = {
+    # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+    stateVersion = "24.05";
     username = "pete";
     homeDirectory = "/home/pete";
     packages =
@@ -138,7 +140,4 @@
     };
     ssh-agent.enable = true;
   };
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.05";
 }
