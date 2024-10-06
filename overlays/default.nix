3668583e418da0b1ef25252bb0416b23288e2155
pete3n <pete3n@protonmail.com>
2024-10-06 10:51:39 -0400

    fix: Add check and throw error for out-of-order overlay use

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,38 +1,22 @@
 # This file defined flake-wide overlays
+{ inputs, build_target, ... }:
 {
-  inputs,
-  build_target,
-  ...
-}: {
   # Import local pkgs from ./packages as overlays.local-packages and prepend
   # them with local to differentiate between nixpkgs version. Call with pkgs.local
   local-packages = final: prev: {
-    local = let
-      pkgs = final;
+    local =
+      let
+        pkgs = final;
 
-      # Import cross-platform packages
-      crossPlatformPackages = import ../packages/cross-platform {
-        inherit pkgs;
-      };
+        # Import cross-platform packages
+        crossPlatformPackages = import ../packages/cross-platform { inherit pkgs; };
 
-      # Import linux-only packages if our target is linux
-      linuxPackages =
-        if build_target.isLinux
-        then
-          import ../packages/linux {
-            inherit pkgs;
-          }
-        else {};
+        # Import linux-only packages if our target is linux
+        linuxPackages = if build_target.isLinux then import ../packages/linux { inherit pkgs; } else { };
 
-      # Import darwin-only packages if our target is darwin
-      darwinPackages =
-        if !build_target.isLinux
-        then
-          import ../packages/darwin {
-            inherit pkgs;
-          }
-        else {};
-    in
+        # Import darwin-only packages if our target is darwin
+        darwinPackages = if !build_target.isLinux then import ../packages/darwin { inherit pkgs; } else { };
+      in
       # Merge all packages into `local`
       crossPlatformPackages // linuxPackages // darwinPackages;
   };
@@ -40,27 +24,33 @@
   # Individual package modifications, can be accessed by applying the
   # outputs.overlays.mod-packages overlay to nixpkgs. Call with pkgs.mod
   mod-packages = final: prev: {
-    mod = {
-      # Workaround for: https://github.com/signalapp/Signal-Desktop/issues/6855
-      # Cannot find target for triple amdgcn--
-      no-gpu-signal-desktop = prev.unstable.signal-desktop.overrideAttrs (oldAttrs: {
-        installPhase =
-          oldAttrs.installPhase
-          + ''
-            wrapProgram $out/bin/signal-desktop \
-            --set LIBGL_ALWAYS_SOFTWARE 1 \
-            --set ELECTRON_DISABLE_GPU true
-          '';
-      });
+    mod =
+      if prev ? unstable && prev ? local then
+        {
+          # Workaround for: https://github.com/signalapp/Signal-Desktop/issues/6855
+          # Cannot find target for triple amdgcn--
+          no-gpu-signal-desktop = prev.unstable.signal-desktop.overrideAttrs (oldAttrs: {
+            installPhase =
+              oldAttrs.installPhase
+              + ''
+                wrapProgram $out/bin/signal-desktop \
+                --set LIBGL_ALWAYS_SOFTWARE 1 \
+                --set ELECTRON_DISABLE_GPU true
+              '';
+          });
 
-      _86Box = prev._86Box-with-roms.overrideAttrs (oldAttrs: {
-        preFixup =
-          oldAttrs.preFixup
-          + ''
-            makeWrapperArgs+=(--set QT_QPA_PLATFORM "xcb")
-          '';
-      });
-    };
+          _86Box = prev._86Box-with-roms.overrideAttrs (oldAttrs: {
+            preFixup =
+              oldAttrs.preFixup
+              + ''
+                makeWrapperArgs+=(--set QT_QPA_PLATFORM "xcb")
+              '';
+          });
+        }
+      else
+        throw "The 'unstable' or 'local' overlay is missing. The mod overlay \
+				may modify unstable or local packages, so it is necessary to apply them \
+				before attempting to apply the mod overlay.";
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
