3cdf1d712c4f9d9c724547d6f1a422157ed8fedf
pete3n <pete3n@protonmail.com>
2024-10-12 16:25:55 -0400

    chore: consolidated cross-platform programs

--- home-manager/pete/home-imports/cross-platform/cli-programs.nix
+++ home-manager/pete/home-imports/cross-platform/cli-programs.nix
@@ -7,83 +7,79 @@ let
   };
 in
 {
-  # Local wallpaper-scripts module for changing wallpapers
-  wallpaper-scripts = {
-    enable = true;
-    os = if build_target.isLinux then "linux" else "darwin";
-  };
-
-  # Better cat
-  bat = {
-    enable = true;
-    config = {
-      theme = "TwoDark";
+  programs = {
+    # Local wallpaper-scripts module for changing wallpapers
+    wallpaper-scripts = {
+      enable = true;
+      os = if build_target.isLinux then "linux" else "darwin";
     };
-    extraPackages = with pkgs.bat-extras; [
-      batdiff
-      batman
-      batgrep
-      batwatch
-    ];
-  };
-  # Better top resource monitor
-  btop = {
-    enable = true;
-    settings = {
-      vim_keys = true;
-      theme_background = false;
-      color_theme = "nord";
+    # Better cat
+    bat = {
+      enable = true;
+      config = {
+        theme = "TwoDark";
+      };
+      extraPackages = with pkgs.bat-extras; [
+        batdiff
+        batman
+        batgrep
+        batwatch
+      ];
     };
-  };
-  # LSDeluxe improved ls command
-  lsd = {
-    enable = true;
-    enableAliases = true;
-  };
-  # Fastfetch neofetch replacement
-  fastfetch = {
-    enable = true;
-  };
-  # Fuzzy finder
-  fzf = {
-    enable = true;
-    inherit shellIntegration;
-  };
-  keychain = {
-    enable = true;
-    inherit shellIntegration;
-    agents = [
-      "ssh"
-      "gpg"
-    ];
-    keys = [ "pete3n" ];
-  };
-  # Starship cross-shell prompt config
-  starship = {
-    enable = true;
-    inherit shellIntegration;
-    settings = {
-      directory = {
-        truncation_length = 0; # Disable truncation to show the full path
+    # Better top resource monitor
+    btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+        theme_background = false;
+        color_theme = "nord";
       };
     };
-  };
-  # Recursive grep
-  ripgrep = {
-    enable = true;
-  };
-  # Yazi cli file manager
-  yazi = {
-    enable = true;
-    inherit shellIntegration;
-  };
-  # Zathura PDF viewer with VIM motions
-  zathura = {
-    enable = true;
-  };
-  # Zoxide better cd replacement with memory
-  zoxide = {
-    enable = true;
-    inherit shellIntegration;
+    # LSDeluxe improved ls command
+    lsd = {
+      enable = true;
+      enableAliases = true;
+    };
+    # Fastfetch neofetch replacement
+    fastfetch = {
+      enable = true;
+    };
+    # Fuzzy finder
+    fzf = {
+      enable = true;
+    } // shellIntegration;
+    keychain = {
+      enable = true;
+      agents = [
+        "ssh"
+        "gpg"
+      ];
+      keys = [ "pete3n" ];
+    } // shellIntegration;
+    # Starship cross-shell prompt config
+    starship = {
+      enable = true;
+      settings = {
+        directory = {
+          truncation_length = 0; # Disable truncation to show the full path
+        };
+      };
+    } // shellIntegration;
+    # Recursive grep
+    ripgrep = {
+      enable = true;
+    };
+    # Yazi cli file manager
+    yazi = {
+      enable = true;
+    } // shellIntegration;
+    # Zathura PDF viewer with VIM motions
+    zathura = {
+      enable = true;
+    };
+    # Zoxide better cd replacement with memory
+    zoxide = {
+      enable = true;
+    } // shellIntegration;
   };
 }
