8c641f1feeaa993d3206b03f7b41b432b6272bf1
pete3n <pete3n@protonmail.com>
2024-10-12 16:26:32 -0400

    chore: migrated cross-platform programs

--- home-manager/pete/linux-home.nix
+++ home-manager/pete/linux-home.nix
@@ -9,6 +9,7 @@
   imports = builtins.attrValues outputs.homeManagerModules ++ [
     ./home-imports/cross-platform/alacritty-config.nix
     ./home-imports/cross-platform/git-config.nix
+    ./home-imports/cross-platform/cli-programs.nix
     ./home-imports/linux/bash-config.nix
     ./home-imports/linux/firefox-config.nix
     ./home-imports/linux/hyprland-config.nix
@@ -147,80 +148,6 @@
   programs = {
     home-manager.enable = true;
 
-    # Local wallpaper-scripts module for changing wallpapers
-    wallpaper-scripts = {
-      enable = true;
-      os = "linux";
-    };
-
-    # Better cat
-    bat = {
-      enable = true;
-      config = {
-        theme = "TwoDark";
-      };
-      extraPackages = with pkgs.bat-extras; [
-        batdiff
-        batman
-        batgrep
-        batwatch
-      ];
-    };
-    # Better top resource monitor
-    btop = {
-      enable = true;
-      settings = {
-        vim_keys = true;
-        theme_background = false;
-        color_theme = "nord";
-      };
-    };
-    # LSDeluxe improved ls command
-    lsd = {
-      enable = true;
-      enableAliases = true;
-    };
-    # Fastfetch neofetch replacement
-    fastfetch = {
-      enable = true;
-    };
-    # Fuzzy finder
-    fzf = {
-      enable = true;
-      enableBashIntegration = true;
-    };
-    keychain = {
-      enable = true;
-      enableBashIntegration = true;
-      agents = [
-        "ssh"
-        "gpg"
-      ];
-      keys = [ "pete3n" ];
-    };
-    # Starship cross-shell prompt config
-    starship = {
-      enable = true;
-      enableBashIntegration = true;
-    };
-    # Recursive grep
-    ripgrep = {
-      enable = true;
-    };
-    # Yazi cli file manager
-    yazi = {
-      enable = true;
-      enableBashIntegration = true;
-    };
-    # Zathura PDF viewer with VIM motions
-    zathura = {
-      enable = true;
-    };
-    # Zoxide better cd replacement with memory
-    zoxide = {
-      enable = true;
-      enableBashIntegration = true;
-    };
     firefox = {
       enable = true;
     };
