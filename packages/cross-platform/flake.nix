447624dae197a94ab7e2c13e420fe95f456ec2d4
pete3n <pete3n@protonmail.com>
2025-01-02 15:41:08 -0500

    feat: AngryOxide cross-platform

--- packages/cross-platform/flake.nix
+++ packages/cross-platform/flake.nix
@@ -9,26 +9,33 @@
   };
 
   outputs =
-    { nixpkgs, angryoxide }:
     {
-      defaultPackage.x86_64-linux =
-        (import nixpkgs {
-          system = "x86_64-linux";
-          crossSystem = {
-            config = "mips-unknown-linux-musl";
-            gcc = {
-              float = "soft";
-            };
-          };
-          overlays = [
-            (self: super: {
-              angryoxide = import angryoxide {
-                inherit (super) lib;
-                fetchFromGitHub = super.fetchFromGitHub;
-                rustPlatform = super.rustPlatform;
+      self,
+      nixpkgs,
+      angryoxide,
+    }:
+    {
+      packages.x86_64-linux.default =
+        let
+          crossPkgs = import nixpkgs {
+            system = "x86_64-linux";
+            crossSystem = {
+              config = "mips-unknown-linux-musl";
+              gcc = {
+                float = "soft";
               };
-            })
-          ];
-        }).pkgsStatic.file;
+            };
+            overlays = [
+              (self: super: {
+                angryoxide = import angryoxide {
+                  inherit (super) lib;
+                  fetchFromGitHub = super.fetchFromGitHub;
+                  rustPlatform = super.rustPlatform;
+                };
+              })
+            ];
+          };
+        in
+        crossPkgs.angryoxide;
     };
 }
