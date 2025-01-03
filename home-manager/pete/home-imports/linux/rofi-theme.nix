21da79c2a36048dc49723fe0133266ebe8cac763
pete3n <pete3n@protonmail.com>
2025-01-03 11:25:32 -0500

    chore: moved rofi theme to separate module file

--- home-manager/pete/home-imports/linux/rofi-theme.nix
+++ home-manager/pete/home-imports/linux/rofi-theme.nix
@@ -1,152 +1,206 @@
-# This is the ugly way to create a dotfile when there are no
-# supported module options
-{ ... }:
+{ config, lib, ... }:
+let
+  inherit (config.lib.formats.rasi) mkLiteral;
+in
 {
-  home.file.".config/rofi/config.rasi".text = ''
-    @theme "/dev/null"
-    * {
-        selected-normal-foreground:  rgba ( 255, 255, 255, 100 % );
-        foreground:                  rgba ( 255, 255, 255, 100 % );
-        normal-foreground:           @foreground;
-        alternate-normal-background: transparent;
-        red:                         rgba ( 220, 50, 47, 100 % );
-        selected-urgent-foreground:  rgba ( 255, 195, 156, 100 % );
-        blue:                        rgba ( 38, 139, 210, 100 % );
-        urgent-foreground:           rgba ( 243, 132, 61, 100 % );
-        alternate-urgent-background: transparent;
-        active-foreground:           rgba ( 38, 139, 210, 100 % );
-        lightbg:                     rgba ( 238, 232, 213, 100 % );
-        selected-active-foreground:  rgba ( 32, 81, 113, 100 % );
-        alternate-active-background: transparent;
-        background:                  transparent;
-        bordercolor:                 rgba ( 57, 57, 57, 100 % );
-        alternate-normal-foreground: @foreground;
-        normal-background:           transparent;
-        lightfg:                     rgba ( 88, 104, 117, 100 % );
-        selected-normal-background:  rgba ( 38, 139, 210, 100 % );
-        border-color:                @foreground;
-        spacing:                     2;
-        separatorcolor:              rgba ( 38, 139, 210, 100 % );
-        urgent-background:           transparent;
-        selected-urgent-background:  rgba ( 38, 139, 210, 100 % );
-        alternate-urgent-foreground: @urgent-foreground;
-        background-color:            rgba ( 0, 0, 0, 0 % );
-        alternate-active-foreground: @active-foreground;
-        active-background:           rgba ( 10, 0, 71, 100 % );
-        selected-active-background:  rgba ( 38, 139, 210, 100 % );
-    }
-    window {
-        background-color: rgba ( 57, 57, 57, 80 % );
-        border:           1;
-        padding:          5;
-    }
-    mainbox {
-        border:  0;
-        padding: 0;
-    }
-    message {
-        border:       1px dash 0px 0px ;
-        border-color: @separatorcolor;
-        padding:      1px ;
-    }
-    textbox {
-        text-color: @foreground;
-    }
-    listview {
-        fixed-height: 0;
-        border:       2px dash 0px 0px ;
-        border-color: @separatorcolor;
-        spacing:      2px ;
-        scrollbar:    true;
-        padding:      2px 0px 0px ;
-    }
-    element {
-        border:  0;
-        padding: 1px ;
-    }
-    element-text {
-        background-color: inherit;
-        text-color:       inherit;
-    }
-    element.normal.normal {
-        background-color: @normal-background;
-        text-color:       @normal-foreground;
-    }
-    element.normal.urgent {
-        background-color: @urgent-background;
-        text-color:       @urgent-foreground;
-    }
-    element.normal.active {
-        background-color: @active-background;
-        text-color:       @active-foreground;
-    }
-    element.selected.normal {
-        background-color: @selected-normal-background;
-        text-color:       @selected-normal-foreground;
-    }
-    element.selected.urgent {
-        background-color: @selected-urgent-background;
-        text-color:       @selected-urgent-foreground;
-    }
-    element.selected.active {
-        background-color: @selected-active-background;
-        text-color:       @selected-active-foreground;
-    }
-    element.alternate.normal {
-        background-color: @alternate-normal-background;
-        text-color:       @alternate-normal-foreground;
-    }
-    element.alternate.urgent {
-        background-color: @alternate-urgent-background;
-        text-color:       @alternate-urgent-foreground;
-    }
-    element.alternate.active {
-        background-color: @alternate-active-background;
-        text-color:       @alternate-active-foreground;
-    }
-    scrollbar {
-        width:        4px ;
-        border:       0;
-        handle-width: 8px ;
-        padding:      0;
-    }
-    mode-switcher {
-        border:       2px dash 0px 0px ;
-        border-color: @separatorcolor;
-    }
-    button.selected {
-        background-color: @selected-normal-background;
-        text-color:       @selected-normal-foreground;
-    }
-    button {
-        background-color: @background;
-        text-color:       @foreground;
-    }
-    inputbar {
-        spacing:    0;
-        text-color: @normal-foreground;
-        padding:    1px ;
-    }
-    case-indicator {
-        spacing:    0;
-        text-color: @normal-foreground;
-    }
-    entry {
-        spacing:    0;
-        text-color: @normal-foreground;
-    }
-    prompt {
-        spacing:    0;
-        text-color: @normal-foreground;
-    }
-    inputbar {
-        children:   [ prompt,textbox-prompt-colon,entry,case-indicator ];
-    }
-    textbox-prompt-colon {
-        expand:     false;
-        str:        ":";
-        margin:     0px 0.3em 0em 0em ;
-        text-color: @normal-foreground;
-    }
-  '';
+  options.rofi.theme = lib.mkOption {
+    type = lib.types.attrsOf lib.types.anything;
+    default = { };
+    description = "Rofi theme configuration.";
+  };
+  config = {
+    rofi.theme = {
+      "*" = {
+        selected-normal-foreground = mkLiteral "#ffffff";
+        foreground = mkLiteral "#ffffff";
+        normal-foreground = mkLiteral "@foreground";
+        alternate-normal-background = mkLiteral "transparent";
+        red = mkLiteral "#ff322f";
+        selected-urgent-foreground = mkLiteral "#ffc39c";
+        blue = mkLiteral "#278bd2";
+        urgent-foreground = mkLiteral "#f3843d";
+        alternate-urgent-background = mkLiteral "transparent";
+        active-foreground = mkLiteral "#268bd2";
+        lightbg = mkLiteral "#eee8d5";
+        selected-active-foreground = mkLiteral "#205171";
+        alternate-active-background = mkLiteral "transparent";
+        background = mkLiteral "transparent";
+        bordercolor = mkLiteral "#393939";
+        alternate-normal-foreground = mkLiteral "@foreground";
+        normal-background = mkLiteral "transparent";
+        lightfg = mkLiteral "#586875";
+        selected-normal-background = mkLiteral "#268bd2";
+        border-color = mkLiteral "@foreground";
+        spacing = mkLiteral "2";
+        separatorcolor = mkLiteral "#268bdb";
+        urgent-background = mkLiteral "transparent";
+        selected-urgent-background = mkLiteral "#268bd2";
+        alternate-urgent-foreground = mkLiteral "@urgent-foreground";
+        background-color = mkLiteral "#00000000";
+        alternate-active-foreground = mkLiteral "@active-foreground";
+        active-background = mkLiteral "#0a0047";
+        selected-active-background = mkLiteral "#268bd2";
+      };
+
+      # Holds the entire window
+      "window" = {
+        background-color = mkLiteral "#393939cc";
+        border = mkLiteral "1";
+        padding = mkLiteral "5";
+      };
+
+      # Wrapper around bar and results
+      "mainbox" = {
+        border = mkLiteral "0";
+        padding = mkLiteral "0";
+      };
+
+      "textbox" = {
+        text-color = mkLiteral "@foreground";
+      };
+
+      # Command prompt left of the input
+      "#prompt" = {
+        enabled = false;
+      };
+
+      # Actual text box
+      "#entry" = {
+        placeholder-color = mkLiteral "#00ff00";
+        expand = true;
+        horizontal-align = "0";
+        placeholder = "";
+        padding = mkLiteral "0px 0px 0px 5px";
+        blink = true;
+      };
+
+      # Top bar
+      "#inputbar" = {
+        children = map mkLiteral [
+          "prompt"
+          "entry"
+        ];
+        border = mkLiteral "1px";
+        border-radius = mkLiteral "4px";
+        padding = mkLiteral "6px";
+      };
+
+      # Results
+      "listview" = {
+        fixed-height = mkLiteral "0";
+        border = mkLiteral "2px dash 0px 0px";
+        border-color = mkLiteral "@separatorcolor";
+        spacing = mkLiteral "2px";
+        scrollbar = mkLiteral "true";
+        padding = mkLiteral "2px 0px 0px";
+      };
+
+      # Each result
+      "element" = {
+        border = mkLiteral "0";
+        padding = mkLiteral "1px";
+      };
+
+      "element.normal.normal" = {
+        background-color = mkLiteral "@normal-background";
+        text-color = mkLiteral "@normal-foreground";
+      };
+
+      "element.normal.urgent" = {
+        background-color = mkLiteral "@urgent-background";
+        text-color = mkLiteral "@urgent-foreground";
+      };
+
+      "element.normal.active" = {
+        background-color = mkLiteral "@active-background";
+        text-color = mkLiteral "@active-foreground";
+      };
+
+      "element.selected.normal" = {
+        background-color = mkLiteral "@selected-normal-background";
+        text-color = mkLiteral "@selected-normal-foreground";
+      };
+
+      "element.selected.urgent" = {
+        background-color = mkLiteral "@selected-urgent-background";
+        text-color = mkLiteral "@selected-urgent-foreground";
+      };
+
+      "element.selected.active" = {
+        background-color = mkLiteral "@selected-active-background";
+        text-color = mkLiteral "@selected-active-foreground";
+      };
+
+      "element.alternate.normal" = {
+        background-color = mkLiteral "@alternate-normal-background";
+        text-color = mkLiteral "@alternate-normal-foreground";
+      };
+
+      "element.alternate.urgent" = {
+        background-color = mkLiteral "@alternate-urgent-background";
+        text-color = mkLiteral "@alternate-urgent-foreground";
+      };
+
+      "element.alternate.active" = {
+        background-color = mkLiteral "@alternate-active-background";
+        text-color = mkLiteral "@alternate-active-foreground";
+      };
+
+      "scrollbar" = {
+        witdh = mkLiteral "4px";
+        border = mkLiteral "0";
+        handle-width = mkLiteral "8px";
+        padding = mkLiteral "0";
+      };
+
+      "mode-switcher" = {
+        border = mkLiteral "2px dash 0px 0px";
+        border-color = mkLiteral "@separatorcolor";
+      };
+
+      "button.selected" = {
+        background-color = mkLiteral "@selected-normal-background";
+        text-color = mkLiteral "@selected-normal-foreground";
+      };
+
+      "button" = {
+        background-color = mkLiteral "@selected-normal-background";
+        text-color = mkLiteral "@selected-normal-foreground";
+      };
+
+      "inputbar" = {
+        spacing = mkLiteral "0";
+        text-color = mkLiteral "@normal-foreground";
+        padding = mkLiteral "1px";
+        children = mkLiteral "[ prompt,textbox-prompt-colon,entry,case-indicator ]";
+      };
+
+      "case-indicator" = {
+        spacing = mkLiteral "0";
+        text-color = mkLiteral "@normal-foreground";
+      };
+
+      "entry" = {
+        spacing = mkLiteral "0";
+        text-color = mkLiteral "@normal-foreground";
+      };
+
+      "prompt" = {
+        spacing = mkLiteral "0";
+        text-color = mkLiteral "@normal-foreground";
+      };
+
+      "textbox-prompt-colon" = {
+        expand = mkLiteral "false";
+        str = ":";
+        margin = mkLiteral "0px 0.3em 0em 0em";
+        text-color = mkLiteral "@normal-foreground";
+      };
+
+      "element-text" = {
+        background-color = mkLiteral "inherit";
+        text-color = mkLiteral "inherit";
+      };
+    };
+  };
 }
