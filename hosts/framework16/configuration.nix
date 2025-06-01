6476fa079825556ebe3358152f05f1e04ddecc16
pete3n <pete3n@protonmail.com>
2025-06-01 11:26:27 -0400

    feat: switch power management

--- hosts/framework16/configuration.nix
+++ hosts/framework16/configuration.nix
@@ -107,17 +107,7 @@
   # Power, thermals
   services = {
     thermald.enable = true;
-    auto-cpufreq.enable = true;
-    auto-cpufreq.settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
-    };
+		power-profiles-daemon.enable = true;
   };
 
   # Audio
