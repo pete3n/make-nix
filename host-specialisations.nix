66d200409ddd40c457505a264677358ef0a5cee9
pete3n <pete3n@protonmail.com>
2025-07-22 13:42:41 -0400

    chore: remove old specialisations.nix

--- host-specialisations.nix
+++
@@ -1,7 +0,0 @@
-{ host }:
-
-let
-  flake = builtins.getFlake (toString ./.);
-  hostConfig = flake.outputs.nixosConfigurations.${host};
-in
-builtins.attrNames (hostConfig.config.specialisation or { })
