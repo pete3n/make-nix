88ea4d2412efd5a3a08fc73d19a200cc09897c90
pete3n <pete3n@protonmail.com>
2025-07-22 17:36:16 -0400

    fix: move tmux zsh config

--- home-manager/pete/home-imports/darwin/zsh-config.nix
+++ home-manager/pete/home-imports/darwin/zsh-config.nix
@@ -4,26 +4,72 @@
     let
       check_fastfetch = # sh
         ''
-          	# Show fastfetch at login but not for every new TMUX pane/window
-          	if [ -z "$FASTFETCH_EXECUTED" ] && [ -z "$TMUX" ]; then
-          		export FASTFETCH_EXECUTED=1
-          		command -v ${pkgs.fastfetch}/bin/fastfetch &> /dev/null &&
-          		${pkgs.fastfetch}/bin/fastfetch
-          	fi
+          # Show fastfetch at login but not for every new TMUX pane/window
+          if [ -z "$FASTFETCH_EXECUTED" ] && [ -z "$TMUX" ]; then
+          	export FASTFETCH_EXECUTED=1
+          	command -v ${pkgs.fastfetch}/bin/fastfetch &> /dev/null &&
+          	${pkgs.fastfetch}/bin/fastfetch
+          fi
         '';
 
       no_compfix = # sh
         ''
-          	# Ignore unsafe directory warnings from Darwin
-          	ZSH_DISABLE_COMPFIX="true"
+          # Ignore unsafe directory warnings from Darwin
+          ZSH_DISABLE_COMPFIX="true"
+        '';
+
+      tmux_ssh_wrapper = # sh
+        ''
+                    # Change the Tmux window name based on the SSH destination host
+          					# to more easily track open connections
+                      ssh() {
+                      	echo "Executing custom ssh wrapper"
+                      	# Check if inside tmux
+                      	if ps -p $$ -o ppid= | xargs -I {} ps -p {} -o comm= | grep -qw tmux; then
+                      		# Save the current window name so we can restore it
+                      		local original_window_name=$(tmux display-message -p '#W')
+
+                      		reset_window_name() {
+                      			tmux rename-window "$original_window_name"
+                      		}
+
+                      		# Trap SIGINT to handle ^C
+                      		trap reset_window_name SIGINT
+
+                      		# Parse and extract the destination host with some really ugly character matching
+                      		local destination=$(echo "$@" | sed 's/[[:space:]]*\(\(\(-[46AaCfGgKkMNnqsTtVvXxYy]\)\|\(-[^[:space:]]*\([[:space:]]\+[^[:space:]]*\)\?\)\)[[:space:]]*\)*[[:space:]]\+\([^-][^[:space:]]*\).*/\6/')
+
+                      		tmux rename-window "$destination"
+
+                      		# Execute the SSH command with original args
+                      		command ssh "$@"
+                      		local ssh_exit_status=$?
+
+                      		# Reset trap to default
+                      		trap - SIGINT
+
+                      		# Check if SSH session exited normally
+                      		if [ $ssh_exit_status -ne 0 ]; then
+                      			# SSH failed or was terminated, revert to the original window name
+                      			tmux rename-window "$original_window_name"
+                      		else
+                      			# SSH session ended normally, reset window name to automatic
+                      			tmux set-window-option automatic-rename "on" 1> /dev/null
+                      		fi
+                      	else
+                      		# We aren't in a tmux session, just run SSH with original args
+                      		command ssh "$@"
+                      	fi
+                      }
         '';
 
       earlyInit = lib.mkOrder 550 ''
-        	${no_compfix}
+        ${no_compfix}
       '';
 
       afterInit = lib.mkOrder 1000 ''
-        ${check_fastfetch}
+                ${check_fastfetch}
+        				${tmux_ssh_wrapper}
       '';
     in
     {
