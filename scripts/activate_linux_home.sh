a9fec07a5105fc5e457cdb45c6e9e03ed6e1a449
pete3n <pete3n@protonmail.com>
2025-07-24 23:52:21 -0400

    build: automated commit by make-nix to keep tree clean

---
+++ scripts/activate_linux_home.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -eu
+# shellcheck disable=SC1091
+. "$(dirname "$0")/ansi.env"
+
+# shellcheck disable=SC1091
+. "$(dirname "$0")/installer.env"
+
+user="${ACTIVATE_NIX_USER:? error: user must be set.}"
+host="${ACTIVATE_NIX_HOST:? error: host must be set.}"
+
+if [ -n "${DRY_RUN+x}" ]; then
+	printf "\n%bDry-run%b %benabled%b: skipping home activiation...\n" "$BLUE" "$RESET" "$GREEN" "$RESET"
+	exit 0
+else
+	printf "\nActivating home-manager configuration...\n"
+	printf "nix run nixpkgs#home-manager -- switch -b backup --flake .#%s@%s" "$user" "$host"
+	if script -q -c true >/dev/null 2>&1; then
+		script -q -c "nix run nixpkgs#home-manager -- switch -b backup --flake .#${user}@${host}" "$LOG_PATH"
+	else
+		nix run nixpkgs#home-manager -- switch -b backup --flake ".#${user}@${host}" | tee "$LOG_PATH"
+	fi
+fi
---
+++ scripts/build_linux_home.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+set -eu
+# shellcheck disable=SC1091
+. "$(dirname "$0")/ansi.env"
+# shellcheck disable=SC1091
+. "$(dirname "$0")/installer.env"
+
+user=$BUILD_NIX_USER
+host=$BUILD_NIX_HOST
+
+if [ -n "${DRY_RUN+x}" ]; then
+	printf "\n%bDry-run%b %benabled%b: configuration will not be activated.\n" "$BLUE" "$RESET" "$GREEN" "$RESET"
+	printf "Building home-manager config for Linux...\n"
+	if script -q -c true >/dev/null 2>&1; then
+		script -q -c "nix run nixpkgs#home-manager -- build -b backup --dry-run --flake .#${user}@${host}" "$LOG_PATH"
+	else
+		nix run nixpkgs#home-manager -- build -b backup --dry-run --flake ".#${user}@${host}" | tee "$LOG_PATH"
+	fi
+else
+	printf "Building home-manager configuration for Linux...\n"
+	if script -q -c true >/dev/null 2>&1; then
+		script -q -c "nix run nixpkgs#home-manager -- build -b backup --flake .#${user}@${host}" "$LOG_PATH"
+	else
+		nix run nixpkgs#home-manager -- build -b backup --flake ".#${user}@${host}" | tee "$LOG_PATH"
+	fi
+fi
