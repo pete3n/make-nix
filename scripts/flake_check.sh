0dc114a539eb1f83d7bf31d2b86aafb3c5935ce6
pete3n <pete3n@protonmail.com>
2025-07-25 00:59:10 -0400

    build: automated commit by make-nix to keep tree clean

---
+++ scripts/flake_check.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC1091
+. "$(dirname "$0")/installer.env"
+
+if script -q -c true >/dev/null 2>&1; then
+	script -q -c "nix flake check --all-systems --extra-experimental-features \
+		'nix-command flakes'" "$LOG_PATH"
+else
+	nix flake check --all-systems --extra-experimental-features 'nix-command flakes' | tee "$LOG_PATH"
+fi
