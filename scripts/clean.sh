0a8c9bd171bcd40e5a601f65f94308a21205ab70
pete3n <pete3n@protonmail.com>
2025-07-26 18:22:27 -0400

    test: env trace

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env sh
 set -eu
-env_file="${MAKE_NIX_ENV:?environment file was not set! Ensure mktemp working and in your path.}"
 
 printf "Clean: env file is: %s\n" "$MAKE_NIX_ENV"
-
-# shellcheck disable=SC1090
-. "$env_file"
 
 if [ -n "${KEEP_LOGS+x}" ]; then
 	exit 0
