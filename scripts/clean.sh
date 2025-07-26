609221ab6c3fc1a5904ffaa92851046499e9be2b
pete3n <pete3n@protonmail.com>
2025-07-26 17:34:01 -0400

    fix: undefined env in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -9,14 +9,14 @@ if [ -n "${KEEP_LOGS+x}" ]; then
 	exit 0
 fi
 
-if [ -f "$MAKE_NIX_LOG" ]; then
+if [ -f "${MAKE_NIX_LOG:-}" ]; then
 	rm -f "$MAKE_NIX_LOG"
 fi
 
-if [ -f "$MAKE_NIX_ENV" ]; then
+if [ -f "${MAKE_NIX_ENV:-}" ]; then
 	rm -f "$MAKE_NIX_ENV"
 fi
 
-if [ -f "$MAKE_NIX_INSTALLER" ]; then
+if [ -f "${MAKE_NIX_INSTALLER:-}" ]; then
 	rm -f "$MAKE_NIX_INSTALLER"
 fi
