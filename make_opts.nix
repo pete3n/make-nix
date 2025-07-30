4c52a59f90317eaa2d516fd408ccd0f6d151c366
pete3n <pete3n@protonmail.com>
2025-07-30 15:54:08 -0400

    build: Make-nix automated commit to keep git tree clean

---
+++ make_opts.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  user = "pete";
+  host = "framework16";
+  system = "x86_64-linux";
+  isLinux = true;
+  isHomeAlone = false;
+  useCache = false;
+  useKeys = false
+  tags = [ ];
+  specialisations = [ ];
+}
