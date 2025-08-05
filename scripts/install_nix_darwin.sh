f2d7f0d46697cb08b5a50ca03dba80f8a4580420
pete3n <pete3n@protonmail.com>
2025-08-05 01:52:35 -0400

    fix: host resolution

--- scripts/install_nix_darwin.sh
+++ scripts/install_nix_darwin.sh
@@ -4,6 +4,18 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR/common.sh"
 
+
+if [ -z "${TGT_HOST:-}" ]; then
+	host="$(uname -n)"
+	if [ -z "$host" ]; then
+		logf "\n%berror:%b could not determine local hostname.\n" "$RED" "$RESET"
+		exit 1 
+	fi
+	printf "TGT_HOST=%s\n" "$host" >> "$MAKE_NIX_ENV"
+else
+	host=$TGT_HOST
+fi
+
 clobber_list="nix/nix.conf zshenv zshrc bashrc"
 restored=false
 restoration_list=""
@@ -47,8 +59,8 @@ if [ -f "$nix_conf_backup" ]; then
 fi
 
 logf "%binfo:%b installing with command\n"
-logf "sudo nix run --option experimental-features \"nix-command flakes\" --option trusted-substituters \"$substituters\" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake .#%s" "$TGT_HOST"
-if sudo nix run --option experimental-features "nix-command flakes" --option trusted-substituters \""$substituters"\" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake .#"$TGT_HOST"; then
+logf "sudo nix run --option experimental-features \"nix-command flakes\" --option trusted-substituters \"$substituters\" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake .#%s" "$host"
+if sudo nix run --option experimental-features "nix-command flakes" --option trusted-substituters \""$substituters"\" nix-darwin/nix-darwin-25.05#darwin-rebuild -- switch --flake .#"$host"; then
   logf "\n%b✓ Nix-Darwin install succeeded.%b\n" "$GREEN" "$RESET"
   # Prevent restoration on trap
   restoration_list=""
