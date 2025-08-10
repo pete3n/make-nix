aade32786917689c88ab0d50cacb89514e405fc3
pete <pete3n@protonmail.com>
2025-08-10 11:35:43 -0400

    refactor home libs

--- lib/home-path.nix
+++ lib/home-path.nix
@@ -1,15 +1,26 @@
+# Resolve correct home module path given a base path, system, and user.
+# ./<base>/<user>/<platform>/home.nix
 { lib }:
-{ system, user }:
+{
+  basePath,
+  system,
+  user,
+}:
+assert builtins.pathExists basePath || throw "getHomePath: base filepath not found: ${toString basePath}";
+assert builtins.readDir basePath || throw "getHomePath: basePath is not a directory: ${toString basePath}";
+assert (system != "") || throw "getHomePath: system was not passed.";
+assert (user != "") || throw "getHomePath: user was not passed.";
+
 let
   platform =
-    if lib.strings.hasSuffix "-darwin" system then "darwin"
-    else if lib.strings.hasSuffix "-linux" system then "linux"
-    else throw "Unsupported system: ${system}";
-
-  root      = ../users/homes;  # Static path so Nix can evaluate it
-  home_path = root + "/${user}/${platform}/home.nix";
+    if lib.strings.hasSuffix "-darwin" system then
+      "darwin"
+    else if lib.strings.hasSuffix "-linux" system then
+      "linux"
+    else
+      throw "getHomePath: unsupported system '${system}'";
+  homePath = basePath + "/${user}/${platform}/home.nix";
 in
-  assert (user != "") || throw "Empty user not allowed";
-  assert builtins.pathExists home_path
-    || throw "Home-manager module not found: ${toString home_path}";
-  home_path
+assert
+  builtins.pathExists homePath || throw "Home-manager module not found: ${toString homePath}";
+homePath
