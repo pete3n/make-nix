b8e4a94ff5b6fa2aae7ab09a98b1f98e82e7dfd8
pete3n <pete3n@protonmail.com>
2025-08-14 07:51:18 -0400

    fix: don't trap exit

--- scripts/set_spec_boot.sh
+++ scripts/set_spec_boot.sh
@@ -4,8 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR/common.sh"
 
-trap 'cleanup $? EXIT' EXIT
-trap 'cleanup 130 SIGNAL' INT TERM QUIT   # one generic non-zero code for signals
+trap 'cleanup 130 SIGNAL' INT TERM QUIT # one generic non-zero code for signals
 
 if is_truthy "${DRY_RUN:-}" || ! is_truthy "${BOOT_SPEC:-}"; then
 	exit 0
