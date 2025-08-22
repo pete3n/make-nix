1182a02a65e02552198368c62b2edae570626765
make-nix <make-nix@bot>
2025-08-22 09:48:53 -0400

    build: Make-nix automated commit to keep git tree clean

--- make-attrs/system/pete@framework16.nix
+++ make-attrs/system/pete@framework16.nix
@@ -5,8 +5,8 @@
   system = "x86_64-linux";
   isHomeAlone = false;
   useHomebrew = false;
-  useCache = true;
-  useKeys = true;
-  tags = [ "hyprland" "superuser" ];
-  specialisations = [ "wayland" "wayland_egpu" "x11" "x11_egpu" ];
+  useCache = false;
+  useKeys = false;
+  tags = [ ];
+  specialisations = [ ];
 }
---
+++ modules/home-manager/cross-platform/quick_notes.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.programs.quickNotes;
+
+  quickNotes =
+    pkgs.writeShellScriptBin "qn"
+      #sh
+        ''
+          	#!/usr/bin/env sh
+          	
+          	# Usage: qn.sh "Any quoted text string.\n Newline \t tab."
+          	notes_file=quick_notes.md
+          	notes_path="$\{XDG_DOCUMENTS_DIR:-$HOME/Documents}/$notes_file"
+          	
+          	[ -d "$(dirname "$notes_path")" ] || exit 1
+          	input="$*"
+          	printf "%b\n" "$input" >> "$notes_path"
+        '';
+in
+{
+  options.programs.quickNotes = {
+    enable = lib.mkEnableOption "Install the quickNotes scripts";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ quickNotes ];
+  };
+}
