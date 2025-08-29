132cbe85bba21aaed2b3a50bb68a0b951bd519ee
pete3n <pete3n@protonmail.com>
2025-08-29 13:17:06 -0400

    fix: quick notes path

--- modules/home-manager/cross-platform/quick_notes.nix
+++ modules/home-manager/cross-platform/quick_notes.nix
@@ -7,16 +7,15 @@
 
 let
   cfg = config.programs.quickNotes;
+	DLR = "$";
 
   quickNotes =
     pkgs.writeShellScriptBin "qn"
       #sh
         ''
-          	#!/usr/bin/env sh
-          	
           	# Usage: qn.sh "Any quoted text string.\n Newline \t tab."
           	notes_file=quick_notes.md
-          	notes_path="$\{XDG_DOCUMENTS_DIR:-$HOME/Documents}/$notes_file"
+          	notes_path="${DLR}{XDG_DOCUMENTS_DIR:-$HOME/Documents}/$notes_file"
           	
           	[ -d "$(dirname "$notes_path")" ] || exit 1
           	input="$*"
