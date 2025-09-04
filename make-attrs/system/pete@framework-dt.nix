aada7d3e162851885531210a09da1888e4c35d34
make-nix <make-nix@bot>
2025-09-04 08:37:35 -0400

    build: Make-nix automated commit to keep git tree clean

---
+++ make-attrs/system/pete@framework-dt.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+  user = "pete";
+  host = "framework-dt";
+  system = "x86_64-linux";
+  isHomeAlone = false;
+  useHomebrew = false;
+  useCache = true;
+  useKeys = true;
+  tags = [ "poweruser" "hyprland" ];
+  specialisations = [ "wayland" "wayland_egpu" ];
+}
