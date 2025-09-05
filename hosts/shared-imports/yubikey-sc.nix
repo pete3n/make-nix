cdb9f4d74cf524dae5ff11bfd82e5f6c99ee6008
pete3n <pete3n@protonmail.com>
2025-09-05 17:52:28 -0400

    fix: multi-host u2f config

--- hosts/shared-imports/yubikey-sc.nix
+++ hosts/shared-imports/yubikey-sc.nix
@@ -21,6 +21,7 @@
   # mkdir -p ~/.config/Yubico
   # pamu2fcfg > ~/.config/Yubico/u2f_keys
   # pamu2fcfg -n >> ~/.config/Yubico/u2f_keys (For additional keys)
+  # This is provided in the user configuration.
   # WARNING: Don't set these to true until keys are configured:
   security.pam = {
     services = {
@@ -31,8 +32,14 @@
       enable = true;
       control = "required";
       settings = {
+        # Fixed realm for multiple hosts using same key
+        origin = "pam://p22";
+        appid = "pam://p22";
         debug = false;
         cue = true;
+        authFile = "%h/.config/u2f_keys";
+        openasuser = true;
+        expand = true;
       };
     };
   };
