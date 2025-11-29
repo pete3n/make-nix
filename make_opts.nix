abf270dc7d9d69622bff7beb29989929acee5878
make-nix <make-nix@bot>
2025-11-29 17:42:04 -0500

    build: Make-nix automated commit to keep git tree clean

--- make_opts.nix
+++ make_opts.nix
@@ -1,12 +1,13 @@
 { ... }:
 {
   user = "pete";
-  host = "DESKTOP-GVHI4H3";
+  host = "framework16";
   system = "x86_64-linux";
   isLinux = true;
-  isHomeAlone = true;
+  isHomeAlone = false;
+  useHomebrew = false;
   useCache = true;
   useKeys = true;
-  tags = [ "superuser" ];
-  specialisations = [ ];
+  tags = [ "hyprland" "poweruser" ];
+  specialisations = [ "wayland" ];
 }
