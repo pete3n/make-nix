dd429bbca4c5edf1dcebeb9c39f0ed8a091bcaf4
pete3n <pete3n@protonmail.com>
2025-12-02 05:25:48 -0500

    feat: battery-minder module import

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -3,4 +3,5 @@
   lazydocker = import ./cross-platform/lazydocker.nix;
   clip58 = import ./cross-platform/clip58.nix;
 	backup = import ./cross-platform/backup.nix;
+	battery-minder = import ./linux/battery-minder.nix;
 }
