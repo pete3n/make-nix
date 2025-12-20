326f9869ed1e43c99543acf048a0d9594904c03d
pete3n <pete3n@protonmail.com>
2025-12-20 18:53:41 -0500

    feat: dgpu specialisation

--- hosts/framework16/specialisations.nix
+++ hosts/framework16/specialisations.nix
@@ -12,6 +12,7 @@ let
     "x11_egpu"
     "wayland"
     "wayland_egpu"
+    "wayland_dgpu"
 		"wayland_pwr_save"
   ];
 
@@ -24,6 +25,7 @@ let
     x11_egpu = import ./specialisations/x11_egpu.nix { inherit lib pkgs outputs; };
     wayland = import ./specialisations/wayland.nix { inherit lib pkgs outputs; };
     wayland_egpu = import ./specialisations/wayland_egpu.nix { inherit lib pkgs outputs; };
+    wayland_dgpu = import ./specialisations/wayland_dgpu.nix { inherit lib pkgs outputs; };
 		wayland_pwr_save = import ./specialisations/wayland_pwr_save.nix { inherit lib pkgs outputs; };
   };
 
