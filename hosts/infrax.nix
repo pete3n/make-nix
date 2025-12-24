302a181ede5946523c33c5cd359ecaad92a4d67e
pete3n <pete3n@protonmail.com>
2025-12-23 22:12:20 -0500

    fix: cache server config

--- hosts/infrax.nix
+++ hosts/infrax.nix
@@ -8,15 +8,21 @@
         # for more information.
         "http://backupsvr.p22:8000"
         "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
       ];
       trusted-substituters = [
         "http://backupsvr.p22:8000"
         "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
       ];
     })
     (lib.mkIf makeNixAttrs.useKeys {
       # Verify at: https://app.cachix.org/cache/nix-community#pull
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+			# Verify at: https://github.com/NixOS/nixpkgs/blob/1f949558617ebb18bbf7005c1c4dc3407d391e93/nixos/modules/services/misc/nix-daemon.nix#L806
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
     })
   ];
 }
