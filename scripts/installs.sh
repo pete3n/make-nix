b65bcf724536ec0847e929b703087dcf8945fbe7
pete3n <pete3n@protonmail.com>
2025-12-29 21:29:36 -0500

    fix: Nix detection bug

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -151,7 +151,7 @@ _launch_nix_install() {
 
 if ! has_cmd "nix"; then
 	source_nix
-	if ! has_cmd "nix"; then
+	if has_cmd "nix"; then
 		logf "\n%binfo:%b Nix already installed. Skipping install...\n" "${C_INFO}" "${C_RST}"
 		logf "If you want to re-install, please run 'make uninstall' first.\n"
 	else
