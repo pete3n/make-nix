b156b114681e5949716c2faaa7286e629542a598
make-nix <make-nix@bot>
2025-12-31 01:18:48 -0500

    build: Make-nix automated commit to keep git tree clean

---
+++ make-attrs/system/pete@test.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  user = "pete";
+  host = "test";
+  system = "x86_64-linux";
+  isLinux = true;
+  isHomeAlone = false;
+  useHomebrew = false;
+  useCache = false;
+  useKeys = false;
+  tags = [ ];
+  specialisations = [ ];
+}
