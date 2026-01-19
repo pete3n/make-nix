53a4757c4340c87208ea66f12cb1d2250b918c4d
pete3n <pete3n@protonmail.com>
2026-01-18 20:44:03 -0500

    fix: sshd pam config

--- hosts/shared-imports/pam-sshd.nix
+++ hosts/shared-imports/pam-sshd.nix
@@ -1,18 +1,30 @@
 # Customize pam for SSH server
-{ lib, pkgs, ... }:
+{ config, pkgs, lib, makeNixAttrs, ... }:
+
+let
+  homeDir = config.users.users.${makeNixAttrs.user}.home or "/home/${makeNixAttrs.user}";
+  u2f_keyfile = "${homeDir}/.config/Yubico/u2f_keys";
+  u2f = config.security.pam.u2f.settings;
+in
 {
-  security.pam.services = {
-    sudo.u2fAuth = lib.mkForce false;
-    pam.services.sudo.text = lib.mkForce ''
-      # If this is an SSH session (pts/*), skip U2F.
+  security.pam.services.sudo = {
+    u2fAuth    = lib.mkForce false;
+    fprintAuth = lib.mkForce false;
+
+    text = lib.mkForce ''
+      # If this is an SSH session (pts/*), skip the next line (U2F)
       auth [success=1 default=ignore] ${pkgs.linux-pam}/lib/security/pam_succeed_if.so tty =~ ^/dev/pts/
 
       # Local console sudo requires YubiKey
       auth required ${pkgs.pam_u2f}/lib/security/pam_u2f.so \
-        cue origin=pam://p22 appid=pam://p22 \
-        authfile=%h/.config/Yubico/u2f_keys openasuser=1
+        cue \
+        origin=${u2f.origin} \
+        appid=${u2f.appid} \
+        authfile=${u2f_keyfile} \
+        openasuser \
+        expand
 
-      # SSH sudo falls back to password (if allowed)
+      # SSH sudo fallback 
       auth required ${pkgs.linux-pam}/lib/security/pam_unix.so try_first_pass
 
       account required ${pkgs.linux-pam}/lib/security/pam_unix.so
