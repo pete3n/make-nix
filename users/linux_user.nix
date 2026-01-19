872136feb379173527fbfa88f92bf93dc9f1a2cf
pete3n <pete3n@protonmail.com>
2026-01-18 20:44:13 -0500

    feat: ssh user

--- users/linux_user.nix
+++ users/linux_user.nix
@@ -6,6 +6,7 @@ let
   userRoleTags = [
     "sudoer"
     "poweruser"
+    "sshuser"
   ];
 
   availableTags = builtins.filter (tag: builtins.elem tag userRoleTags) makeNixAttrs.tags;
@@ -14,6 +15,8 @@ let
     sudoer = [
       "wheel"
     ];
+
+    sshuser = [ ];
 
     poweruser = [
       "networkmanager"
@@ -26,17 +29,28 @@ let
   tagDescriptionMap = {
     sudoer = "User with sudo (wheel) access.";
     poweruser = "Sudoer also with netman, docker, and adbuser membership.";
+    sshuser = "Assign ssh keys.";
   };
+
+  userSshKeys = [
+    # Primary Yubikey
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEFU2BKDdywiMqeD7LY8lgKeBo0mjHEyP7ej+Y2JNuJDAAAABHNzaDo= pete@framework16"
+    # Backup Yubikey
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHwNQ411TYRwGAGINX4i4FI7Ek7lfTQv0s8vbXmnqVh/AAAABHNzaDo= pete@framework16"
+  ];
 
   tagRoleGroups = lib.flatten (builtins.map (tag: tagGroupMap.${tag}) availableTags);
   tagRoleDescription = lib.concatStringsSep "; " (
     builtins.map (tag: tagDescriptionMap.${tag}) availableTags
   );
+
+  hasTag = t: builtins.elem t availableTags;
 in
 {
   users.users.${makeNixAttrs.user} = {
     isNormalUser = true;
     description = tagRoleDescription;
     extraGroups = tagRoleGroups;
+    openssh.authorizedKeys.keys = lib.mkIf hasTag "sshuser" userSshKeys;
   };
 }
