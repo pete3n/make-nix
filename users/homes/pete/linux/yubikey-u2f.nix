78064734bb067e23013e9a9104c6ffe14847250b
pete3n <pete3n@protonmail.com>
2026-01-19 20:03:37 -0500

    style: formatting

--- users/homes/pete/linux/yubikey-u2f.nix
+++ users/homes/pete/linux/yubikey-u2f.nix
@@ -1,7 +1,6 @@
+# Imperative creation of Yubikey auth file
 { pkgs, makeNixAttrs, ... }:
 {
-  # Imperative creation of Yubikey auth file
-  # p22 origin and appid
   home.file."/home/${makeNixAttrs.user}/.config/Yubico/u2f_keys" = {
     source = "${pkgs.writeText "${makeNixAttrs.user}-u2f-auth-file" ''${makeNixAttrs.user}:jPXIHluUKJNDbiCSQ5+DRfMrG+ZNqMyQXTHSyByi5XHSXHNhZC2CduqlqNOIutx2NIc8Qhn2omlCFpcOoDjukw==,FQlfOdBDXUlixODcx+4gDsFIyLaX21KWqkEmbVx3ny7iwJpL43O2BRMAcArBJWJ/tEsz2/lxI/gZk7Dn9093vA==,es256,+presence:4a218pdZXDWigFWVcGDubvTbdAN9cAlp9+r0CPezvDojRPeou4j1m6vv4ZqW70jzNhAd9HD4gV0ykhC4Uoxi0A==,ftm749QLZ7sgH9ITIyb+f3Wn4BXDjK32+qIMlkfkOnMZ8On6GWBteaITzdCZ6PRzbTCQPZ6TC+ylGLw/rn0Ewg==,es256,+presence''}";
   };
