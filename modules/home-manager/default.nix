d054b5eb8914ea9690c2329f10287b7633ac0e37
pete3n <pete3n@protonmail.com>
2026-01-19 20:03:11 -0500

    refactor: rename modules

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,9 +1,10 @@
 {
-  wallpaper-scripts = import ./cross-platform/wallpaper-scripts.nix;
-  lazydocker = import ./cross-platform/lazydocker.nix;
-  clip58 = import ./cross-platform/clip58.nix;
 	backup = import ./cross-platform/backup.nix;
 	battery-minder = import ./linux/battery-minder.nix;
+	bootstrap-ssh = import ./cross-platform/bootstrap-ssh.nix;
+  clip58 = import ./cross-platform/clip58.nix;
+  lazydocker = import ./cross-platform/lazydocker.nix;
 	power-profile-switcher = import ./linux/power-profile-switcher.nix;
-	quickNotes = import ./cross-platform/quick_notes.nix;
+	quick-notes = import ./cross-platform/quick-notes.nix;
+  wallpaper-scripts = import ./cross-platform/wallpaper-scripts.nix;
 }
