4f1d43ed82e1951d7019d12a912780cd7b4c9e8a
pete3n <pete3n@protonmail.com>
2026-02-09 08:51:21 -0500

    test: sshPubKeys

--- make-attrs/system/pete@framework-dt.nix
+++ make-attrs/system/pete@framework-dt.nix
@@ -9,5 +9,8 @@
   useCache = false;
   useKeys = false;
   tags = [ "poweruser" "hyprland" "sshuser" ];
+	sshPubKeys = [ 
+		"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH0sKLi0IwMU62lLAEBiPudg4OxqQGY1n3MOsV8rAJybAAAAB3NzaDpwMjI= ssh:p22"
+	];
   specialisations = [ "wayland" "wayland_egpu" ];
 }
