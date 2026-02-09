cd92ecdd83e1dcecc660966aeb17b019f057e25c
make-nix <make-nix@bot>
2026-02-09 08:46:28 -0500

    build: Make-nix automated commit to keep git tree clean

--- make-attrs/system/pete@framework-dt.nix
+++ make-attrs/system/pete@framework-dt.nix
@@ -9,8 +9,5 @@
   useCache = false;
   useKeys = false;
   tags = [ "poweruser" "hyprland" "sshuser" ];
-	sshPubKeys = [ 
-		"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH0sKLi0IwMU62lLAEBiPudg4OxqQGY1n3MOsV8rAJybAAAAB3NzaDpwMjI= ssh:p22"
-	];
   specialisations = [ "wayland" "wayland_egpu" ];
 }
