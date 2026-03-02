d6d1dcce7765cae59090366fd49027360dceffc4
pete3n <pete3n@protonmail.com>
2026-03-01 20:34:27 -0500

    style: formatting

--- hosts/infrax.nix
+++ hosts/infrax.nix
@@ -3,18 +3,21 @@
 {
   nix.settings = lib.mkMerge [
     (lib.mkIf makeNixAttrs.useCache {
-			# This is a local Nginx cache for cache.nixos and
-			# nix-community.cachix.org, see https://github.com/pete3n/nix-cache.git
-			# for more information.
+      # This is a local Nginx cache for cache.nixos and
+      # nix-community.cachix.org, see https://github.com/pete3n/nix-cache.git
+      # for more information.
       substituters = [
         "http://backupsvr.p22:8000/"
         "https://nix-community.cachix.org/"
         "https://cache.nixos.org/"
       ];
+
+      connect-timeout = 2; # seconds (fast fail local cache is unavailable)
+      stalled-download-timeout = 15;
     })
     (lib.mkIf makeNixAttrs.useKeys {
       # Verify at: https://app.cachix.org/cache/nix-community#pull
-			# Verify at: https://github.com/NixOS/nixpkgs/blob/1f949558617ebb18bbf7005c1c4dc3407d391e93/nixos/modules/services/misc/nix-daemon.nix#L806
+      # Verify at: https://github.com/NixOS/nixpkgs/blob/1f949558617ebb18bbf7005c1c4dc3407d391e93/nixos/modules/services/misc/nix-daemon.nix#L806
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
