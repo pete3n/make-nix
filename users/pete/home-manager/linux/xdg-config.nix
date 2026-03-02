20692710053a254dbef12939e7723149d558d127
pete3n <pete3n@protonmail.com>
2026-03-01 20:56:10 -0500

    fix: Dolphin integration, portals, and mime types

--- users/pete/home-manager/linux/xdg-config.nix
+++ users/pete/home-manager/linux/xdg-config.nix
@@ -1,30 +1,40 @@
 { pkgs, makeNixAttrs, ... }:
 {
+  home.packages = with pkgs; [
+    desktop-file-utils
+    kdePackages.kservice # provides kbuildsycoca6
+    kdePackages.kio-extras
+    kdePackages.kmenuedit
+  ];
   xdg = {
     enable = true;
     portal = {
       enable = true;
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+      extraPortals = [
+        pkgs.kdePackages.xdg-desktop-portal-kde
+        pkgs.xdg-desktop-portal-hyprland
+        pkgs.xdg-desktop-portal-gtk
+      ];
       xdgOpenUsePortal = true;
       config = {
         common = {
-          default = [ "gtk" ];
+          default = [
+            "hyprland"
+            "kde"
+          ];
+        };
+        # Route file picking to kde/gtk depending on app toolkit
+        "org.freedesktop.impl.portal.FileChooser" = {
+          default = [ "kde" ];
+        };
+        "org.freedesktop.impl.portal.Screenshot" = {
+          default = [ "hyprland" ];
+        };
+        "org.freedesktop.impl.portal.ScreenCast" = {
+          default = [ "hyprland" ];
         };
       };
     };
-
-    dataFile."kservices6/ServiceMenus/open-alacritty-here.desktop".text = ''
-      [Desktop Entry]
-      Type=Service
-      ServiceTypes=KonqPopupMenu/Plugin
-      MimeType=inode/directory;
-      Actions=OpenAlacrittyHere
-
-      [Desktop Action OpenAlacrittyHere]
-      Name=Open Alacritty Here
-      Icon=utilities-terminal
-      Exec=${pkgs.alacritty}/bin/alacritty --working-directory %f
-    '';
 
     userDirs = {
       enable = true;
@@ -62,5 +72,26 @@
         "video/mpeg" = [ "vlc.desktop" ];
       };
     };
+
+		# Fix for Dolphin to recognize Alacritty
+    configFile."kdeglobals" = {
+      text = ''
+        [General]
+        TerminalApplication=alacritty
+        TerminalService=Alacritty.desktop
+      '';
+    };
+
+		# Fix to allow kbuildsyscoca6 to build the menu database correctly
+    configFile."menus/applications.menu".text = ''
+      <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
+        "http://www.freedesktop.org/standards/menu-spec/menu-1.0.dtd">
+      <Menu>
+        <Name>Applications</Name>
+        <DefaultAppDirs/>
+        <DefaultDirectoryDirs/>
+        <DefaultMergeDirs/>
+      </Menu>
+    '';
   };
 }
