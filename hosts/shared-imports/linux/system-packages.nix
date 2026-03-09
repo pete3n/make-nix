f511483f2cfcc9131ececea7817fcb197281c9d0
pete3n <pete3n@protonmail.com>
2026-03-09 11:53:16 -0400

    chore: organize common linux system packages

---
+++ hosts/shared-imports/linux/system-packages.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+{
+  programs.steam.enable = true;
+  environment.systemPackages =
+    (with pkgs; [
+      acpi
+      auto-cpufreq
+			clinfo
+      cryptsetup
+      dhcpcd
+      dig
+      docker-compose
+      gnumake
+      gparted
+      gptfdisk
+      home-manager
+      iw
+      killall
+      knot-dns
+      libnotify
+      nixos-rebuild-ng
+      openvpn
+      parted
+      pavucontrol
+      pciutils
+      pipewire
+      qemu
+      qemu-utils
+			ragenix
+      tcpdump
+      thermald
+      traceroute
+      usbutils
+      wpa_supplicant
+    ])
+    ++ [
+      (pkgs.unstable.lutris.override {
+        extraPkgs = p: [
+          p.wineWow64Packages.waylandFull
+          p.winetricks
+        ];
+      })
+    ];
+}
