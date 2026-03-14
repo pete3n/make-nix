48c81a3e4d13f4d339a0c27aebbb96fc99b33b6b
pete3n <pete3n@protonmail.com>
2026-03-13 23:43:42 -0400

    feat: remote build config

--- hosts/shared-imports/remote-builder.nix
+++ hosts/shared-imports/remote-builder.nix
@@ -13,17 +13,19 @@
           "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEFU2BKDdywiMqeD7LY8lgKeBo0mjHEyP7ej+Y2JNuJDAAAABHNzaDo= pete@framework16"
           # Backup Yubikey
           "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHwNQ411TYRwGAGINX4i4FI7Ek7lfTQv0s8vbXmnqVh/AAAABHNzaDo= pete@framework16"
+          # Nix daemon builder key
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFIOfV+x8CjUKB0lByreL2NR9IWMYvBwFFYFMvAUpzTG p22-build@nix" # Nix daemon builder key
         ];
       };
     };
   };
 
-  nix.settings.allowed-users = [ 
-		"@builders" 
-		"root" 
-	];
-  nix.settings.trusted-users = [ 
-		"@builders"
-		"root"
-	];
+  nix.settings.allowed-users = [
+    "@builders"
+    "root"
+  ];
+  nix.settings.trusted-users = [
+    "@builders"
+    "root"
+  ];
 }
