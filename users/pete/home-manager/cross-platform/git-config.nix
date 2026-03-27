f755fd4fa696ec9ee9880a3bd5edebb1adffff72
pete3n <pete3n@protonmail.com>
2026-03-27 14:43:20 -0400

    feat: implement yubi-age-decrypt module

--- users/pete/home-manager/cross-platform/git-config.nix
+++ users/pete/home-manager/cross-platform/git-config.nix
@@ -1,16 +1,20 @@
 {
   config,
-  lib,
-  pkgs,
-  makeNixLib,
   makeNixAttrs,
   ...
 }:
-let
-  hasGitUser = makeNixLib.hasTag "git-user" makeNixAttrs.tags;
-  needsHomeActivation = hasGitUser && makeNixAttrs.isHomeAlone;
-in
 {
+  programs.yubi-age-decrypt = {
+    enable = makeNixAttrs.isHomeAlone;
+    secrets = [
+      {
+        outputFile = "${config.home.homeDirectory}/.ssh/pete3n";
+        ageFile = "${../../secrets/pete3n.age}";
+        identityFile = "${../../secrets/age-plugin-yubikeys}";
+      }
+    ];
+  };
+
   programs.git = {
     enable = true;
     settings = {
@@ -39,38 +43,4 @@ in
     [title-min-length]
     min-length=5
   '';
-
-  home.activation = lib.mkIf needsHomeActivation {
-    decryptGitSshKey =
-      lib.hm.dag.entryAfter [ "writeBoundary" ] # sh
-        ''
-          export PATH="${pkgs.age-plugin-yubikey}/bin:${pkgs.age}/bin:${pkgs.pcsclite}/bin:$PATH"
-          export PCSCLITE_CSOCK_NAME="/run/pcscd/pcscd.comm"
-          export LD_LIBRARY_PATH="${pkgs.pcsclite}/lib:$LD_LIBRARY_PATH"
-
-          _ssh_dir="${config.home.homeDirectory}/.ssh"
-          _key_path="$_ssh_dir/pete3n"
-          _age_file="${../../secrets/pete3n.age}"
-          _identity="${../../secrets/age-plugin-yubikeys}"
-
-          mkdir -p "$_ssh_dir"
-          chmod 700 "$_ssh_dir"
-
-          if [ ! -f "$_key_path" ]; then
-          	if ! [ -S /run/pcscd/pcscd.comm ]; then
-          		printf "\033[0;35mwarning: \033[0mpcscd socket not found at /run/pcscd/pcscd.comm. Install and start pcscd, then re-run home-manager switch to decrypt git SSH key."  >&2
-          	elif [ ! -t 0 ]; then
-          		printf "\ndecryptGitSshKey: not a TTY; skipping git SSH key decryption. Re-run home-manager switch with a TTY to decrypt.\n" >&2
-          	else
-          		$DRY_RUN_CMD ${pkgs.age}/bin/age \
-          			--decrypt \
-          			--identity "$_identity" \
-          			--output "$_key_path" \
-          			"$_age_file" \
-          			</dev/tty >/dev/tty 2>/dev/tty
-          		$DRY_RUN_CMD chmod 600 "$_key_path"
-          	fi
-          fi
-        '';
-  };
 }
