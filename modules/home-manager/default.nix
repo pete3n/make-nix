648ef2a52f1a139197d23f83c0c5fffacd99706c
pete3n <pete3n@protonmail.com>
2026-03-27 14:43:07 -0400

    refactor: HM module names

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,14 +1,15 @@
 {
-	backup = import ./cross-platform/backup.nix;
-	hyprWhichKey = import ./linux/hyprWhichKey.nix;
-	import-yubikey-ssh = import ./cross-platform/import-yubikey-ssh.nix;
-	khal-notify = import ./linux/khal-notify.nix;
-  lazydocker = import ./cross-platform/lazydocker.nix;
-	p22Sync = import ./cross-platform/p22-sync.nix;
-	pomodoro = import ./linux/pomodoro-module.nix;
-	power-profile-switcher = import ./linux/power-profile-switcher.nix;
-	quick-notes = import ./cross-platform/quick-notes.nix;
-	yubi-age-secrets = import ./linux/yubi-age-secrets.nix;
+  backup = import ./cross-platform/backup.nix;
   clip58 = import ./cross-platform/clip58.nix;
+  hyprWhichKey = import ./linux/hyprWhichKey.nix;
+  khal-notify = import ./linux/khal-notify.nix;
+  lazydocker = import ./cross-platform/lazydocker.nix;
+  p22Sync = import ./cross-platform/p22-sync.nix;
+  pomodoro = import ./linux/pomodoro-module.nix;
+  power-profile-switcher = import ./linux/power-profile-switcher.nix;
+  quick-notes = import ./cross-platform/quick-notes.nix;
   wallpaper-scripts = import ./cross-platform/wallpaper-scripts.nix;
+  yubi-age-secrets = import ./linux/yubi-age-secrets.nix;
+  yubi-age-decrypt = import ./cross-platform/yubi-age-decrypt.nix;
+  yubi-ssh-import = import ./cross-platform/yubi-ssh-import.nix;
 }
