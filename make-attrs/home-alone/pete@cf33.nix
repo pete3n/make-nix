f7fcd8265f2ec6cb64e3b0b7d17f97e9d1f6bb91
pete3n <pete3n@protonmail.com>
2026-03-27 16:20:56 -0400

    feat: cf33 home-alone config

---
+++ make-attrs/home-alone/pete@cf33.nix
@@ -0,0 +1,14 @@
+{ ... }:
+{
+  user = "pete";
+  host = "cf33";
+  system = "x86_64-linux";
+  isLinux = true;
+  isHomeAlone = true;
+  useHomebrew = false;
+  useCache = false;
+  useKeys = false;
+  tags = [ "hyprland" "power-user" "git-user" "nixvim" "sdr" ];
+  specialisations = [ ];
+  sshPubKeys = [ ];
+}
