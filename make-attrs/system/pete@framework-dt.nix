50fd01f7c2252593fe716a503a005acd37629de8
pete3n <pete3n@protonmail.com>
2026-03-28 07:17:03 -0400

    feat: update framework-dt tags

--- make-attrs/system/pete@framework-dt.nix
+++ make-attrs/system/pete@framework-dt.nix
@@ -8,7 +8,7 @@
   useHomebrew = false;
   useCache = false;
   useKeys = false;
-  tags = [ "crypto" "cuda" "gaming" "git-user" "hyprland" "laptop" "local-ai" "media-creation" "messaging" "mpd" "nixvim" "office" "p22" "power-user" "ssh-user" "yubi-user" ];
+  tags = [ "crypto" "cuda" "gaming" "git-ssh-user" "hyprland" "laptop" "local-ai" "media-creation" "messaging" "mpd" "nixvim" "office" "p22" "power-user" "ssh-user" "yubi-age-user" ];
   specialisations = [ "wayland" "wayland_egpu" ];
   sshPubKeys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH0sKLi0IwMU62lLAEBiPudg4OxqQGY1n3MOsV8rAJybAAAAB3NzaDpwMjI= ssh:p22"
