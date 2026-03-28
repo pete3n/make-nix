4d016a551f7a802afc3a5296b4f456ccbf51579c
pete3n <pete3n@protonmail.com>
2026-03-28 11:14:44 -0400

    fix: macbook attrs

--- make-attrs/system/pete@macbook.nix
+++ make-attrs/system/pete@macbook.nix
@@ -2,10 +2,12 @@
 {
   user = "pete";
   host = "macbook";
-  system = "aarch64-darwin";
-	isLinux = false;
+  system = "x86_64-darwin";
+  isLinux = false;
   isHomeAlone = false;
   useHomebrew = true;
   useCache = true;
   useKeys = true;
+  tags = [ "power-user" ];
+  specialisations = [ ];
 }
