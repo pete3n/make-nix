db1172028b75b25d28508efd1867af9e9d785d8b
pete3n <pete3n@protonmail.com>
2026-03-29 16:44:56 -0400

    feat: restore desktop attrs

---
+++ make-attrs/system/pete@framework-dt.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  user = "pete";
+  host = "framework-dt";
+  system = "x86_64-linux";
+  isLinux = true;
+  isHomeAlone = false;
+  useHomebrew = false;
+  useCache = false;
+  useKeys = false;
+  tags = [ "crypto" "cuda" "gaming" "git-ssh-user" "hyprland" "laptop" "local-ai" "media-creation" "messaging" "mpd" "nixvim" "office" "p22" "power-user" "ssh-user" "yubi-age-user" ];
+  specialisations = [ "wayland" "wayland_egpu" ];
+  sshPubKeys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH0sKLi0IwMU62lLAEBiPudg4OxqQGY1n3MOsV8rAJybAAAAB3NzaDpwMjI= ssh:p22"
+   ];
+}
