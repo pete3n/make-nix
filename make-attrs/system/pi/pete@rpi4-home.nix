ec7df8865903b0f79f502513cca1136050abacf1
make-nix <make-nix@bot>
2026-04-01 17:13:56 -0400

    build: Make-nix automated commit to keep git tree clean

--- make-attrs/system/pi/pete@rpi4-home.nix
+++ make-attrs/system/pi/pete@rpi4-home.nix
@@ -10,7 +10,9 @@
   useKeys = true;
   tags = [ "ssh-user" "sudo-user" "git" ];
   specialisations = [ ];
-  sshPubKeys = [ ];
+  sshPubKeys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH0sKLi0IwMU62lLAEBiPudg4OxqQGY1n3MOsV8rAJybAAAAB3NzaDpwMjI= ssh:p22"
+   ];
   piBoard = "rpi4";
   buildSystem = "x86_64-linux";
   deployMethod = "sd-image";
