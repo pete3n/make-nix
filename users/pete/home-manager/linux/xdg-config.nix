446eaa3b4cc3c8e65eca7c76ba7a17af6cafd7ed
pete3n <pete3n@protonmail.com>
2026-05-31 16:08:23 -0400

    fix: portal configuration

--- users/pete/home-manager/linux/xdg-config.nix
+++ users/pete/home-manager/linux/xdg-config.nix
@@ -1,4 +1,10 @@
-{ pkgs, makeNixAttrs, ... }:
+{
+  lib,
+  pkgs,
+  makeNixAttrs,
+  makeNixLib,
+  ...
+}:
 {
   home.packages = with pkgs; [
     desktop-file-utils
@@ -6,93 +12,94 @@
     kdePackages.kio-extras
     kdePackages.kmenuedit
   ];
-  xdg = {
+  xdg.enable = true;
+  xdg.portal = {
     enable = true;
-    portal = {
-      enable = true;
-      extraPortals = [
-        pkgs.kdePackages.xdg-desktop-portal-kde
-        pkgs.xdg-desktop-portal-hyprland
-        pkgs.xdg-desktop-portal-gtk
-      ];
-      xdgOpenUsePortal = true;
-      config = {
-        common = {
-          default = [
-            "hyprland"
-            "kde"
-          ];
-        };
-        # Route file picking to kde/gtk depending on app toolkit
-        "org.freedesktop.impl.portal.FileChooser" = {
-          default = [ "kde" ];
-        };
-        "org.freedesktop.impl.portal.Screenshot" = {
-          default = [ "hyprland" ];
-        };
-        "org.freedesktop.impl.portal.ScreenCast" = {
-          default = [ "hyprland" ];
-        };
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ]
+    ++ lib.optionals (makeNixLib.hasTag "hyprland" makeNixAttrs.tags) [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.kdePackages.xdg-desktop-portal-kde
+    ];
+    xdgOpenUsePortal = true;
+    config.common.default =
+      if (makeNixLib.hasTag "hyprland" makeNixAttrs.tags) then
+        [
+          "hyprland"
+          "gtk"
+        ]
+      else
+        [ "gtk" ];
+  };
+
+  xdg.configFile."xdg-desktop-portal/hyprland-portals.conf" =
+    lib.mkIf (makeNixLib.hasTag "hyprland" makeNixAttrs.tags)
+      {
+        text = ''
+          [preferred]
+          default=hyprland;gtk
+          org.freedesktop.impl.portal.FileChooser=kde
+          org.freedesktop.impl.portal.Settings=gtk
+        '';
       };
+
+  xdg.userDirs = {
+    enable = true;
+    setSessionVariables = true;
+    documents = "/home/${makeNixAttrs.user}/Documents";
+    download = "/home/${makeNixAttrs.user}/Downloads";
+    music = "/home/${makeNixAttrs.user}/Music";
+    pictures = "/home/${makeNixAttrs.user}/Pictures";
+    publicShare = "/home/${makeNixAttrs.user}/Public";
+    templates = "/home/${makeNixAttrs.user}/Templates";
+    videos = "/home/${makeNixAttrs.user}/Videos";
+
+    extraConfig = {
+      PROJECT = "/home/${makeNixAttrs.user}/Projects";
     };
+  };
 
-    userDirs = {
-      enable = true;
-      setSessionVariables = true;
-      documents = "/home/${makeNixAttrs.user}/Documents";
-      download = "/home/${makeNixAttrs.user}/Downloads";
-      music = "/home/${makeNixAttrs.user}/Music";
-      pictures = "/home/${makeNixAttrs.user}/Pictures";
-      publicShare = "/home/${makeNixAttrs.user}/Public";
-      templates = "/home/${makeNixAttrs.user}/Templates";
-      videos = "/home/${makeNixAttrs.user}/Videos";
-
-      extraConfig = {
-        PROJECT = "/home/${makeNixAttrs.user}/Projects";
-      };
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "video/x-matroska" = [ "vlc.desktop" ];
+      "video/x-msvideo" = [ "vlc.desktop" ];
+      "video/quicktime" = [ "vlc.desktop" ];
+      "video/webm" = [ "vlc.desktop" ];
+      "video/mpeg" = [ "vlc.desktop" ];
     };
-
-    mimeApps = {
-      enable = true;
-      associations.added = {
-        "application/pdf" = [ "org.kde.okular.desktop" ];
-        "video/mp4" = [ "vlc.desktop" ];
-        "video/x-matroska" = [ "vlc.desktop" ];
-        "video/x-msvideo" = [ "vlc.desktop" ];
-        "video/quicktime" = [ "vlc.desktop" ];
-        "video/webm" = [ "vlc.desktop" ];
-        "video/mpeg" = [ "vlc.desktop" ];
-      };
-      defaultApplications = {
-        "application/pdf" = [ "org.kde.okular.desktop" ];
-        "video/mp4" = [ "vlc.desktop" ];
-        "video/x-matroska" = [ "vlc.desktop" ];
-        "video/x-msvideo" = [ "vlc.desktop" ];
-        "video/quicktime" = [ "vlc.desktop" ];
-        "video/webm" = [ "vlc.desktop" ];
-        "video/mpeg" = [ "vlc.desktop" ];
-      };
+    defaultApplications = {
+      "application/pdf" = [ "org.kde.okular.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "video/x-matroska" = [ "vlc.desktop" ];
+      "video/x-msvideo" = [ "vlc.desktop" ];
+      "video/quicktime" = [ "vlc.desktop" ];
+      "video/webm" = [ "vlc.desktop" ];
+      "video/mpeg" = [ "vlc.desktop" ];
     };
+  };
 
-    # Fix for Dolphin to recognize Alacritty
-    configFile."kdeglobals" = {
-      text = ''
-        [General]
-        TerminalApplication=alacritty
-        TerminalService=Alacritty.desktop
-      '';
-    };
-
-    # Fix to allow kbuildsyscoca6 to build the menu database correctly
-    configFile."menus/applications.menu".text = ''
-      <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
-        "http://www.freedesktop.org/standards/menu-spec/menu-1.0.dtd">
-      <Menu>
-        <Name>Applications</Name>
-        <DefaultAppDirs/>
-        <DefaultDirectoryDirs/>
-        <DefaultMergeDirs/>
-      </Menu>
+  # Fix for Dolphin to recognize Alacritty
+  xdg.configFile."kdeglobals" = {
+    text = ''
+      [General]
+      TerminalApplication=alacritty
+      TerminalService=Alacritty.desktop
     '';
   };
+
+  # Fix to allow kbuildsyscoca6 to build the menu database correctly
+  xdg.configFile."menus/applications.menu".text = ''
+    <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
+      "http://www.freedesktop.org/standards/menu-spec/menu-1.0.dtd">
+    <Menu>
+      <Name>Applications</Name>
+      <DefaultAppDirs/>
+      <DefaultDirectoryDirs/>
+      <DefaultMergeDirs/>
+    </Menu>
+  '';
 }
