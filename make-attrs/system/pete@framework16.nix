650f5ea4ecdf05d0cc9aa4f2f64214f9612ce716
pete3n <pete3n@protonmail.com>
2026-06-08 15:13:26 -0400

    style: formatting

--- make-attrs/system/pete@framework16.nix
+++ make-attrs/system/pete@framework16.nix
@@ -8,7 +8,32 @@
   useHomebrew = false;
   useCache = true;
   useKeys = true;
-  tags = [ "aichat" "crypto" "cuda" "gaming" "git-ssh-user" "hyprland" "laptop" "local-ai" "media-creation" "messaging" "mpd" "nixvim" "office" "p22" "power-user" "vpn-user" "yubi-age-user" "yubi-ssh-import" "yubi-u2f" ];
-  specialisations = [ "wayland_dgpu" "wayland_pwr_save" "kmscon" ];
+  tags = [
+    "aichat"
+    "crypto"
+    "cuda"
+    "gaming"
+    "git-ssh-user"
+    "hyprland"
+    "laptop"
+    "local-ai"
+    "media-creation"
+    "messaging"
+    "mpd"
+    "nixvim"
+    "office"
+    "p22"
+    "power-user"
+    "rocm"
+    "vpn-user"
+    "yubi-age-user"
+    "yubi-ssh-import"
+    "yubi-u2f"
+  ];
+  specialisations = [
+    "wayland_dgpu"
+    "wayland_pwr_save"
+    "kmscon"
+  ];
   sshPubKeys = [ ];
 }
