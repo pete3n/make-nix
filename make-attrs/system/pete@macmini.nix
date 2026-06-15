be11f52f0e69630f03333ed7676f8cd2656b1fcd
make-nix <make-nix@bot>
2026-06-14 20:13:35 -0400

    build: Make-nix automated commit to keep git tree clean

--- make-attrs/system/pete@macmini.nix
+++ make-attrs/system/pete@macmini.nix
@@ -8,7 +8,7 @@
   useHomebrew = true;
   useCache = true;
   useKeys = true;
-  tags = [ "aerospace" "git" "nixvim" "power-user" ];
+  tags = [ "aerospace" "git" "nixvim" "p22" "power-user" ];
   specialisations = [ ];
   sshPubKeys = [ ];
 }
